5e8e2f3d87210f2267720d1dee605a05f50e3d6b
dazehinn <dazehinn.yahoo.fr>
2023-02-16 18:12:51 +0100

    Add call to addLikes feature

--- src/modules/meals/desplayMeal.js
+++ src/modules/meals/desplayMeal.js
@@ -1,4 +1,5 @@
 import getdetailsPopup from '../../popup/detailPopup.js';
+import addLikes from '../Likes/addLikes.js';
 
 const desplayMeal = (mealContainerElement, meal) => {
   const mealsList = document.getElementById('meals_list');
@@ -7,6 +8,10 @@ const desplayMeal = (mealContainerElement, meal) => {
   const btn = document.getElementById(`comments_${meal.idMeal}`);
   btn.addEventListener('click', () => {
     getdetailsPopup(meal.idMeal);
+  });
+  const likeBtn = document.querySelector(`#like_${meal.idMeal}`);
+  likeBtn.addEventListener('click', () => {
+    addLikes(meal.idMeal);
   });
 };
 export default desplayMeal;
