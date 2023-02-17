2a2ee2db0c5cfdfa56b5effdc1d45cab3a540192
Fithamlak <justiceofgod01@gmail.com>
2023-02-17 17:13:03 +0300

    desplay logo image with web pack concept by importing it as amodule

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './style.css';
-
+import getLogo from './modules/getLogo.js';
 import { loadMeals } from './modules/meals/featchAndLoadMeals.js';
 
 import createPopup from './popup/popupContainer.js';
 
 window.onload = () => {
+  getLogo();
   loadMeals();
   createPopup();
 };
